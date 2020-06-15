72c5e4145be9138c6b642de78e8c27a1dfdf6190
Ketan Mandlik <ketanmandlik96@gmail.com>
2020-06-15 23:24:36 +0530

    first-commit

--- App.js
+++ App.js
@@ -1,19 +1,70 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  Image,
+  SafeAreaView,
+  Button,
+  Alert,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => {
+    console.log("Text-Clicked");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Hello React Native Session
+      </Text>
+      <TouchableHighlight
+        onPress={() => {
+          console.log("Image Tapped");
+        }}
+      >
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight>
+      <Button
+        color="orange"
+        title="Click Me"
+        onPress={() =>
+          Alert.alert("My Title", "My Message", [
+            {
+              text: "Yes",
+              onPress: () => {
+                console.log("Yes");
+              },
+            },
+            {
+              text: "No",
+              onPress: () => {
+                console.log("No");
+              },
+            },
+          ])
+        }
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
